34e14b4a6ef36611f15382cf9a07bcb4a8e1d4ba
Eprincev.Egor <eprincev.egor@ya.ru>
2021-02-09 23:54:11 +0300

    added docs to token description

--- lib/token/Token.ts
+++ lib/token/Token.ts
@@ -5,12 +5,29 @@ export type TokenConstructor = new (
     position: Position
 ) => Token;
 
+/**
+ * instructions for parsing text into tokens
+ */
 export interface TokenDescription {
+    /**
+     * regex that tests if a character is a given token
+     */
     entry: RegExp;
+    /**
+     * a set of the most popular token symbols to improve performance
+     */
     popularEntry?: string[];
+    /**
+     * constructor to creating a token
+     */
     TokenConstructor: TokenConstructor;
 }
 
+/**
+ * Default token instance,
+ * if there is no more suitable instance.
+ * This token every time will contain only one character.
+ */
 export class Token {
 
     static description: TokenDescription = {
