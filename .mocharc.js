96d599f54ce3fd67e8c3e320de9db0db5b37f6da
Eprincev.Egor <eprincev.egor@ya.ru>
2021-03-13 22:02:19 +0300

    changed mocha/nyc config

--- .mocharc.js
+++ .mocharc.js
@@ -6,6 +6,11 @@ const config = {
     spec: [
         "lib/**/*.spec.ts",
         "example/**/*.spec.ts"
+    ],
+    require: [
+      "source-map-support/register",
+      "tsconfig-paths/register",
+      "ts-node/register"
     ]
 };
 
